b4a39a11dd3a7eb35207847139dc835d31fe2f16
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-03-28 12:09:15 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+//factorial
+// function findFactorial(num) {
+//     let s =1
+//     for (let i = s; i <= num; i++) {
+//         s*=i
+//     }
+//     return s
+// }
+// console.log(findFactorial(5));
+
+//mecaguyn
+// function biggest(a,b,c) {
+//     if(a>b>c){
+//         return a
+//     }else if(a>b<c){
+//         return c
+//     }
+//     return b
+// }
+// console.log(biggest(2981,1,9038494879));
+
+//zangvaci meci index
+
+// function indexOfTheBiggestNumber(arr){
+//     let s = 0
+//     for (let i = 1; i < arr.length; i++) {
+//         if(arr[i]>arr[s]){
+//             s=i
+//         }
+//     }
+//     return s
+// }
+// console.log(indexOfTheBiggestNumber([0,-182791,76435489570,65324789098032,8838]));
+
+// ete gone meky bajanvum e 3 ev 5
+
+// function atLeastOne(...arr) {
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+// console.log(atLeastOne(1, 5, 3));
+
+
+//ete drakan en true
+// function checkIfPositive(...arr){
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]<0){
+//             return false
+//         }        
+//     }
+//     return true
+// }
+// console.log(checkIfPositive(1762384,6273,10,838));
+
+//leetcode
+
+// function addUp(nums,target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         if(nums[i]+nums[i+1]==target){
+//             return [i,i+1]
+//         }
+//     }
+// }
+// console.log(addUp([3,3],6));//wrong right one is on leet
+
+
+//
+// function plusOne(digits) {
+//     let toNum = BigInt(digits.join(""))+BigInt(1)
+//     return  String(toNum).split("").map(Number)
+// }
+// console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]));
+// console.log(Math.sqrt(8));
+
+// function lengthOfLastWord(s) {
+//     let a = s.trim()
+//     let arr = a.split(" ")
+//     return arr[arr.length-1].length;
+// }
+// console.log(lengthOfLastWord("   fly me   to   the moon  "));
+
+
+
+//medium problem of substrings
+function longestSubstringWithoutDublicate(params) {
+    
+}
